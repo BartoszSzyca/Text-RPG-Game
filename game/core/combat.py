cd191731fdc320b2acada89521e0c6b75a976e37
Bartosz (Bitrax) Szyca <Bartosz.Szyca@gmail.com>
2025-03-14 19:33:49 +0100

    feat: update main.py to include location and use core.initiate_combat

--- game/core/combat.py
+++ game/core/combat.py
@@ -1,9 +1,7 @@
 def initiate_combat(player, monster):
-    print("Rozpoczyna się walka!")
-    while player.is_alive() and monster.is_alive():
-        player.attack(monster)
-        if monster.is_alive():
-            monster.attack(player)
+    player.attack(monster)
+    if monster.is_alive():
+        monster.attack(player)
 
 def calculate_damage(attacker, defender):
     return attacker.attack_power - defender.attributes.health
