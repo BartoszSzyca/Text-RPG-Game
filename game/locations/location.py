a8807a5ddcf1aed1ba099802cdc9b07cafdb66a8
Bartosz (Bitrax) Szyca <Bartosz.Szyca@gmail.com>
2025-03-15 14:14:23 +0100

    feat(locations): add NPC management to Location class

--- game/locations/location.py
+++ game/locations/location.py
@@ -2,6 +2,7 @@ class Location:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.npcs = []
 
     def enter(self):
         print(f"Wchodzisz do {self.name}.")
@@ -9,3 +10,9 @@ class Location:
 
     def exit(self):
         print(f"Opuszczasz {self.name}.")
+
+    def add_npc(self, npc):
+        self.npcs.append(npc)
+
+    def get_npcs(self):
+        return self.npcs
