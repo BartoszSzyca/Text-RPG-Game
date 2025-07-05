8b3e680c34484f49d0146ea6fa011f2bf2773091
Bartosz (Bitrax) Szyca <Bartosz.Szyca@gmail.com>
2025-07-05 19:41:46 +0200

    refactor(items): introduce new item hierarchy and capabilities

--- game/items/item.py
+++ game/items/item.py
@@ -1,11 +1,40 @@
-class Item:
-    def __init__(self, name, description, value, attack_bonus=0, defense_bonus=0, health_bonus=0):
-        self.name = name
-        self.description = description
-        self.value = value
-        self.attack_bonus = attack_bonus
-        self.defense_bonus = defense_bonus
-        self.health_bonus = health_bonus
+from actors.entity import Entity
 
-    def __str__(self):
-        return f"{self.name} - {self.description} (Wartość: {self.value})"
+class Item(Entity):
+    def __init__(self, name, description="_", item_type="misc", weight=0.1,
+                 value=1, slot=None, stat_modifiers=None):
+        super().__init__(name, description, weight)
+        self.item_type = item_type
+        self.value = value
+        self.slot = slot
+        self.stat_modifiers = stat_modifiers if stat_modifiers is not None else {}
+
+    # def __str__(self):
+    #     return f"{self.name} - {self.description} (Wartość: {self.value})"
+
+class Weapon(Item):
+    def __init__(self, name, description, damage, slot="main_hand",
+                 stat_modifiers=None):
+        super().__init__(name, description, item_type="weapon", slot=slot,
+                         stat_modifiers=stat_modifiers)
+        self.damage = damage
+
+class Armor(Item):
+    def __init__(self, name, description, armor_value, slot="chest",
+                 stat_modifiers=None):
+        super().__init__(name, description, item_type="armor", slot=slot,
+                         stat_modifiers=stat_modifiers)
+        self.armor_value = armor_value
+
+class AmuletOfVigor(Item):
+    def __init__(self, name, description, armor_value, slot="chest",
+                 stat_modifiers=None):
+        super().__init__(name, description, item_type="amulet", slot="amulet",
+                         stat_modifiers=stat_modifiers)
+
+class Potion(Item):
+    def __init__(self, name, description, potion_type="healing",
+                 effect_amount=0):
+        super().__init__(name, description, item_type="potion")
+        self.potion_type = potion_type
+        self.effect_amount = effect_amount
